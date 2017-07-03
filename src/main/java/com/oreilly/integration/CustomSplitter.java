caa486844e85ba0f3e325167d019b280eca43115
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-07-03 14:26:43 +0600

    Without using Abstract Type.

--- src/main/java/com/oreilly/integration/CustomSplitter.java
+++ src/main/java/com/oreilly/integration/CustomSplitter.java
@@ -1,18 +1,16 @@
 package com.oreilly.integration;
 
-import org.springframework.integration.splitter.AbstractMessageSplitter;
 import org.springframework.messaging.Message;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Monjur-E-Morshed on 03-Jul-17.
  */
-public class CustomSplitter extends AbstractMessageSplitter {
-
-  @Override
-  protected Object splitMessage(Message<?> message) {
+public class CustomSplitter {
+  public List<String> splitMessage(Message<?> message) {
     return new ArrayList<String>(Arrays.asList(message.getPayload().toString().split(" ")));
   }
 }

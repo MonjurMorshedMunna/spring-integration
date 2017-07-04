5d940bd1445bb35bfd5eae62c9dfa840897ff31f
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-07-04 16:16:39 +0600

    Asynchronous gateway implemented.

--- src/main/java/com/oreilly/integration/EnhancedPrinterGateway.java
+++ src/main/java/com/oreilly/integration/EnhancedPrinterGateway.java
@@ -1,10 +1,12 @@
 package com.oreilly.integration;
 
+import org.springframework.util.concurrent.ListenableFuture;
+
 /**
  * Created by Monjur-E-Morshed on 04-Jul-17.
  */
 public interface EnhancedPrinterGateway {
   public void print(Person person);
 
-  public String uppercase(Person person);
+  public ListenableFuture<String> uppercase(Person person);
 }
--- src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
+++ src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @SpringBootApplication
 @Configuration
@@ -25,7 +27,20 @@ public class SpringIntegrationApplication implements ApplicationRunner {
   public void run(ApplicationArguments pApplicationArguments) throws Exception {
     Person[] payloads = {new Person("Kevin", "Bowsersox"), new Person("Monjur", "Morshed"), new Person("Jane", "Doe'")};
     for (int x = 0; x < payloads.length; x++) {
-      this.gateway.print(payloads[x]);
+      System.out.println("Invoking the gateway method");
+      ListenableFuture<String> future = this.gateway.uppercase(payloads[x]);
+      future.addCallback(new ListenableFutureCallback<String>() {
+        @Override
+        public void onFailure(Throwable pThrowable) {
+
+        }
+
+        @Override
+        public void onSuccess(String pS) {
+          System.out.println("Invoking the success callback");
+          System.out.println(pS);
+        }
+      });
 //      System.out.println(returnMessage);
     }
 

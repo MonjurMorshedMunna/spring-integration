d08f14443e200062f7533e3e9d63d4bc8f3acf9f
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-07-04 10:55:25 +0600

    Custom transformer (second string first, first string second)

--- src/main/java/com/oreilly/integration/CustomTransformer.java
+++ src/main/java/com/oreilly/integration/CustomTransformer.java
@@ -6,8 +6,8 @@ import org.springframework.messaging.Message;
  * Created by Monjur-E-Morshed on 04-Jul-17.
  */
 public class CustomTransformer {
-  public String transform(Message<String> message){
+  public String transform(Message<String> message) {
     String[] tokens = message.getPayload().split(" ");
-    return tokens[1]+", "+tokens[0];
+    return tokens[1] + ", " + tokens[0];
   }
 }

38943ed0247cb870240eba6cd689e5375b9dd0ff
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-07-04 10:53:00 +0600

    Initial transformer integration.

---
+++ src/main/java/com/oreilly/integration/CustomTransformer.java
@@ -0,0 +1,13 @@
+package com.oreilly.integration;
+
+import org.springframework.messaging.Message;
+
+/**
+ * Created by Monjur-E-Morshed on 04-Jul-17.
+ */
+public class CustomTransformer {
+  public String transform(Message<String> message){
+    String[] tokens = message.getPayload().split(" ");
+    return tokens[1]+", "+tokens[0];
+  }
+}

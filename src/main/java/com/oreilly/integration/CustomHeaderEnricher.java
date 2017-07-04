eff1fdf86d6b321a437377dd39e8add1977d6c19
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-07-04 12:45:09 +0600

    Custom header enricher added.

---
+++ src/main/java/com/oreilly/integration/CustomHeaderEnricher.java
@@ -0,0 +1,10 @@
+package com.oreilly.integration;
+
+/**
+ * Created by Monjur-E-Morshed on 04-Jul-17.
+ */
+public class CustomHeaderEnricher {
+  public String getHeaderValue() {
+    return "This is the header value";
+  }
+}

42591bde8eb636a86c8d9cf37cec21d676719ba9
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-07-04 16:39:40 +0600

    Inbound channel adapter implemented

---
+++ src/main/java/com/oreilly/integration/PersonDirectoryService.java
@@ -0,0 +1,11 @@
+package com.oreilly.integration;
+
+/**
+ * Created by Monjur-E-Morshed on 04-Jul-17.
+ */
+public class PersonDirectoryService {
+
+  public Person findNewPeople() {
+    return new Person("Marium", "Shorna");
+  }
+}
--- src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
+++ src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -1,22 +1,16 @@
 package com.oreilly.integration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @SpringBootApplication
 @Configuration
 @ImportResource("integration-context.xml")
 public class SpringIntegrationApplication implements ApplicationRunner {
-
-  @Autowired
-  private EnhancedPrinterGateway gateway;
 
 
   public static void main(String[] args) {
@@ -25,24 +19,6 @@ public class SpringIntegrationApplication implements ApplicationRunner {
 
   @Override
   public void run(ApplicationArguments pApplicationArguments) throws Exception {
-    Person[] payloads = {new Person("Kevin", "Bowsersox"), new Person("Monjur", "Morshed"), new Person("Jane", "Doe'")};
-    for (int x = 0; x < payloads.length; x++) {
-      System.out.println("Invoking the gateway method");
-      ListenableFuture<String> future = this.gateway.uppercase(payloads[x]);
-      future.addCallback(new ListenableFutureCallback<String>() {
-        @Override
-        public void onFailure(Throwable pThrowable) {
-
-        }
-
-        @Override
-        public void onSuccess(String pS) {
-          System.out.println("Invoking the success callback");
-          System.out.println(pS);
-        }
-      });
-//      System.out.println(returnMessage);
-    }
 
   }
 }

3be47bb398d975a18b88b3f8e0816f934111a910
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-07-05 13:48:41 +0600

    reading filex implemented.

---
+++ src/main/java/com/oreilly/integration/FilePrinter.java
@@ -0,0 +1,31 @@
+package com.oreilly.integration;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by Monjur-E-Morshed on 05-Jul-17.
+ */
+public class FilePrinter {
+  public void print(File file) {
+    BufferedReader reader = null;
+
+    try {
+      reader = new BufferedReader(new FileReader(file));
+      String line = "";
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        reader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
